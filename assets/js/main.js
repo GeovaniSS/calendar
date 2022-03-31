9027fd1258d715fb42c6aeb1f66a4ac7dfa4aaa8
GeovaniSS <geosilvasousa@gmail.com>
2022-03-30 23:31:35 -0300

    ✨ feat: previous month and next month navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,33 @@
+let nav = 0
+const prev = document.querySelector('.prev')
+const next = document.querySelector('.next')
+
 const data = new Date()
 const weekDays = ['DOM', 'SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SAB'] // Dias da Semana
 const monthDaysElement = document.querySelector('.month-days') // Calendário
+const currentDateElement = document.querySelector('.current-date')
+
+const clearMonthDays = () => {
+  monthDaysElement.innerText = ''
+}
+
+const loadCurrentDate = () => {
+  const year = data.getFullYear()
+  const month = data.getMonth()
+  nav = month
+
+  currentDateElement.innerText = getCurrentDate(year, month)
+
+  loadMonthDays(year, month)
+}
+
+const updateDate = (month) => {
+  const currentDateElement = document.querySelector('.current-date')
+  currentDateElement.innerText = getCurrentDate(2022, month)
+
+  clearMonthDays()
+  loadMonthDays(2022, month)
+}
 
 const currentWeekDay = () => {
   const weekDaysElement = document.querySelectorAll('.week-day')
@@ -12,30 +39,29 @@ const currentWeekDay = () => {
   })
 }
 
-const loadPreviousMonthDays = (year, previousMonth, currentMonth) => {
-  const totalPreviousMonthDays = getTotalMonthDays(year, previousMonth) 
-  const firstWeekDay = getfirstWeekDay(year, currentMonth) - 1 
-
-  for (let i = totalPreviousMonthDays - firstWeekDay; i <= totalPreviousMonthDays; i++) {
-    const previousMonthDayElement = createPreviousMonthDayElement(i)
-    monthDaysElement.appendChild(previousMonthDayElement)
-  }
-}
-
-const createPreviousMonthDayElement = day => {
-  const previousMonthDayElement = document.createElement('div')
-  previousMonthDayElement.innerText = day
-  previousMonthDayElement.classList.add('previous-month--day')
-
-  return previousMonthDayElement
-}
-
 const loadMonthDays = (year, month) => {
   const totalMonthDays = getTotalMonthDays(year, month)
+  const totalPreviousMonthDays = getTotalMonthDays(year, month - 1)
+  const firstWeekDay = getfirstWeekDay(year, month) - 1 
+  const lastWeekDay = getLastWeekDay(year, month) + 1
 
+  for (let i = totalPreviousMonthDays - firstWeekDay; i <= totalPreviousMonthDays; i++) {
+    const previousMonthDayElement = createMonthDayElement(i)
+    previousMonthDayElement.classList.add('prev-date')
+    monthDaysElement.appendChild(previousMonthDayElement)
+  }
+  
   for (let i = 1; i <= totalMonthDays; i++) {
     const monthDayElement = createMonthDayElement(i)
+    const currentDay = data.getDate()
+    if(i === currentDay) monthDayElement.classList.add('current-month--day')
     monthDaysElement.appendChild(monthDayElement)
+  }
+
+  for (let i = 1; i <= weekDays.length - lastWeekDay; i++) {
+    const nextMonthDayElement = createMonthDayElement(i)
+    nextMonthDayElement.classList.add('next-date')
+    monthDaysElement.appendChild(nextMonthDayElement)
   }
 }
 
@@ -43,49 +69,37 @@ const createMonthDayElement = day => {
   const monthDayElement = document.createElement('div')
   monthDayElement.innerText = day
 
-  const currentDay = data.getDate()
-  if(day === currentDay) monthDayElement.classList.add('current-month--day')
-
   return monthDayElement
 }
 
-const loadNextMonthDays = (year, month) => {
-  const lastWeekDay = getLastWeekDay(year, month) + 1
-
-  for (let i = 1; i <= weekDays.length - lastWeekDay; i++) {
-    const nextMonthDayElement = createNextMonthDayElement(i)
-    monthDaysElement.appendChild(nextMonthDayElement)
-  }
-}
-
-const createNextMonthDayElement = day => {
-  const nextMonthDayElement = document.createElement('div')
-  nextMonthDayElement.innerText = day
-  nextMonthDayElement.classList.add('previous-month--day')
-
-  return nextMonthDayElement
+const getCurrentDate = (year, month) => {
+  const currentDate = new Date(year, month)
+  return currentDate.toLocaleDateString('pt-BR', {month: 'long', year: 'numeric'})
 }
 
 const getTotalMonthDays = (year, month) => {
-  const totalMonthDays = new Date(year, month, 0)
+  const totalMonthDays = new Date(year, month + 1, 0)
   return totalMonthDays.getDate()
 } 
 
 const getfirstWeekDay = (year, month) => {
-  const firstWeekDay = new Date(year, month - 1, 1)
+  const firstWeekDay = new Date(year, month, 1)
   return firstWeekDay.getDay()
 }
 
 const getLastWeekDay = (year, month) => {
-  const lastWeekDay = new Date(year, month, 0)
+  const lastWeekDay = new Date(year, month + 1, 0)
   return lastWeekDay.getDay()
 }
 
-currentWeekDay()
-loadPreviousMonthDays(2022, 2, 3)
-loadMonthDays(2022, 3)
-loadNextMonthDays(2022, 3)
+prev.addEventListener('click', () => {
+  nav--
+  updateDate(nav)
+})
+next.addEventListener('click', () => {
+  nav++
+  updateDate(nav)
+})
 
-// console.log(getTotalMonthDays(2022, 3))
-// console.log(getfirstWeekDay(2022, 3))
-// console.log(getLastWeekDay(2022, 3))
+loadCurrentDate()
+currentWeekDay()
